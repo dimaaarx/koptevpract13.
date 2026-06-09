c4039973066eed60621e8034680a7ade52451c3c
dimaaarx <koptevdima1107@gmail.com>
2026-06-10 02:23:41 +0300

    Update UserIdentifier.java

--- src/UserIdentifier.java
+++ src/UserIdentifier.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
+import java.util.Objects;
 
-public class UserIdentifier {
+public class UserIdentifier implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+
+    public UserIdentifier(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        UserIdentifier that = (UserIdentifier) obj;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
